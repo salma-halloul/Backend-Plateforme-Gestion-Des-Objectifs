3e2c54f6de3c6107cbb376bd0d805d746245d13c
nada.achour <nada.achour@proxym-it.com>
2023-08-02 14:25:49 +0100

    * fix signup issue

--- src/main/java/com/example/springjwt/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/example/springjwt/SpringBootSecurityJwtApplication.java
@@ -3,15 +3,13 @@ package com.example.springjwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+
 @SpringBootApplication
 @EnableConfigurationProperties
-@EntityScan(basePackages = {"com.example.springjwt.models"})  // scan JPA entities
+@EntityScan(basePackages = {"com.example.springjwt.models"})
 public class SpringBootSecurityJwtApplication {
 
     private static ConfigurableApplicationContext applicationContext;
@@ -20,20 +18,4 @@ public class SpringBootSecurityJwtApplication {
         SpringBootSecurityJwtApplication.applicationContext = SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
     }
 }
-/*public class SpringBootSecurityJwtApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
-    }
-
-  *//*  @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(javax.sql.DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("com.example.springjwt.models");
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-        return em;
-    }
-*//*
-}*/
